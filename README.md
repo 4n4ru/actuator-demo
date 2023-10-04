fb6de1d5e25c73a5726580253b2cd817b7fc95e9
Ana Runje <ana.runje@clover.com>
2023-10-04 15:55:37 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # actuator-demo
-Following actuator tutorial by Dan Vega
+Following actuator tutorial by Dan Vega [here](https://www.danvega.dev/blog/2023/09/17/spring-boot-actuator/)
---
+++ src/main/java/dev/runje/ana/actuatordemo/ActuatorDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.runje.ana.actuatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorDemoApplication.class, args);
+	}
+
+}
